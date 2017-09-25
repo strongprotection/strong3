777c263c8b3998b4c8e49a4c88f35db2a48ac661
strongprotection <behnam1898@yahoo.com>
2017-09-25 16:54:32 +0330

    BEHNAM MARZJI

--- strong.sh
+++ strong.sh
@@ -1,56 +1,93 @@
+#!/usr/bin/env bash
+
 cd $HOME/strong3
+
 install() {
-cd tg
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt-get install g++-4.7 -y c++-4.7 -y
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
-sudo apt-get install screen -y
-sudo apt-get install tmux -y
-sudo apt-get install libstdc++6 -y
-sudo apt-get install lua-lgi -y
-sudo apt-get install libnotify-dev -y
-wget https://valtman.name/files/telegram-cli-1222
-mv telegram-cli-1222 tgcli
-chmod +x tgcli
-cd ..
-chmod +x bot
-chmod +x tg
+	    cd tg
+		sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+		sudo apt-get install g++-4.7 -y c++-4.7 -y
+		sudo apt-get update
+		sudo apt-get upgrade
+		sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
+		sudo apt-get install screen -y
+		sudo apt-get install tmux -y
+		sudo apt-get install libstdc++6 -y
+		sudo apt-get install lua-lgi -y
+		sudo apt-get install libnotify-dev -y
+		wget https://valtman.name/files/telegram-cli-1222
+		mv telegram-cli-1222 tgcli
+		chmod +x tgcli
+		cd ..
+		chmod +x bot
+		chmod +x tg
+}
+
+function print_logo() {
+	green " ___  ____  ____   ____          ____ "
+    green "|___   ||  |____| |    | | \  | |  __ "           
+    green " ___|  ||  |  \   |____| |  \ | |_ __|"
+    green   "Developer : @Sudo_StrongProtection"
+	echo -e "\n\e[0m"
 }
 
 function logo_play() {
-declare -A txtlogo
-seconds="0.010"
-txtlogo[1]=" ___  ____  ____   ____          ____ "
-txtlogo[2]="|___   ||  |____| |    | | \  | |  __ "
-txtlogo[3]=" ___|  ||  |  \   |____| |  \ | |_ __|"
-txtlogo[4]="Developer : @Sudo_StrongProtection"
-printf "\e[31m\t"
-for i in ${!txtlogo[@]}; do
-for x in `seq 0 ${#txtlogo[$i]}`; do
-printf "${txtlogo[$i]:$x:1}"
-sleep $seconds
-done
-printf "\n\t"
-done
-printf "\n"
-echo -e "\e[0m"
+    declare -A txtlogo
+    seconds="0.010"
+    txtlogo[1]=" ___  ____  ____   ____          ____ "
+    txtlogo[2]="|___   ||  |____| |    | | \  | |  __ "
+    txtlogo[3]=" ___|  ||  |  \   |____| |  \ | |_ __|"
+    txtlogo[4]="Developer : @Sudo_StrongProtection"
+    printf "\e[31m\t"
+    for i in ${!txtlogo[@]}; do
+        for x in `seq 0 ${#txtlogo[$i]}`; do
+            printf "${txtlogo[$i]:$x:1}"
+            sleep $seconds
+        done
+        printf "\n\t"
+    done
+    printf "\n"
+	echo -e "\e[0m"
 }
 
+function STRONGPROTECTION() {
+	echo -e "\e[0m"
+	green "Developer : @Sudo_StrongProtection"
+	green "CHanell : StrongProtection"
+	white "STRONGPROTECTION ROBOT BY BEHNAM MARZJI"
+	white "Developer : @Sudo_StrongProtection"
+	red   "CHanell : StrongProtection"
+	red   "STRONGPROTECTION ROBOT BY BEHNAM MARZJI"
+	echo -e "\e[0m"
+}
+
+red() {
+  printf '\e[1;31m%s\n\e[0;39;49m' "$@"
+}
+green() {
+  printf '\e[1;32m%s\n\e[0;39;49m' "$@"
+}
+white() {
+  printf '\e[1;37m%s\n\e[0;39;49m' "$@"
+}
 update() {
-git pull
+	git pull
 }
 
 if [ "$1" = "install" ]; then
-logo_play
-install
-else
+	print_logo
+	STRONGPROTECTION
+	logo_play
+	install
+  else
 if [ ! -f ./tg/tgcli ]; then
-echo "tg not found"
-echo "Run $0 install"
-exit 1
-fi
-logo_play
-./tg/tgcli -s ./bot/bot.lua -l 1 -E $@
+    echo "tg not found"
+    echo "Run $0 install"
+    exit 1
+ fi
+	print_logo
+	STRONGPROTECTION
+	logo_play
+   #sudo service redis-server restart
+   ./tg/tgcli -s ./bot/bot.lua -l 1 -E $@
+   #./tg/tgcli -s ./bot/bot.lua $@
 fi
